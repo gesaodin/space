9522b96e56098f2aeb336cfa9b3001ea239f8045
Carlos Peña <gesaodin@gmail.com>
2016-11-17 23:32:52 -0400

    Paquete de Intereses

---
+++ application/modules/panel/models/beneficiario/Intereses/CalculoInteres.php
@@ -0,0 +1,22 @@
+<?php 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * MamonSoft 
+ *
+ * Grado
+ *
+ * @package pace\application\modules\panel\model\beneficiario
+ * @subpackage utilidad
+ * @author Carlos Peña
+ * @copyright Derechos Reservados (c) 2015 - 2016, MamonSoft C.A.
+ * @link http://www.mamonsoft.com.ve
+ * @since version 1.0
+ */
+
+class CalculoInteres extends CI_Model{
+
+
+}
+
