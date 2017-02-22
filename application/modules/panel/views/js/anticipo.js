f35d172a2de0000a1c6df9251ab6eb428638016a
Carlos Peña <gesaodin@gmail.com>
2017-02-22 17:40:47 -0400

    #Anticipo: CargarFecha

--- application/modules/panel/views/js/anticipo.js
+++ application/modules/panel/views/js/anticipo.js
@@ -58,10 +58,10 @@ function consultar() {
             $("#sexo").val(data.sexo);
             $("#componente").val(data.Componente.nombre);
             $("#grado").val(data.Componente.Grado.nombre);
-            $("#fingreso").val(data.fecha_ingreso);
+            $("#fingreso").val(cargarFecha(data.fecha_ingreso));
             $("#tservicio").val(data.tiempo_servicio);
             $("#nhijos").val(data.numero_hijos);
-            $("#fuascenso").val(data.fecha_ultimo_ascenso);
+            $("#fuascenso").val(cargarFecha(data.fecha_ultimo_ascenso));
             $("#noascenso").val(data.no_ascenso);
             $("#profesionalizacion").val(data.profesionalizacion);
             $("#arec").val(data.ano_reconocido);
@@ -133,6 +133,13 @@ function consultar() {
         limpiar();
     });
     //limpiar();
+}
+
+function cargarFecha(fecha){
+    if(fecha != null){
+      var f = fecha.split('-');
+      return f[2] + '/' + f[1] + '/' + f[0];
+    }
 }
 
 function listar(data){
