0268a939527412da0eeec18c05a85c647438f63e
mercy <cardenasmercy@gmail.com>
2017-04-04 04:44:03 -0400

    Script configurar

--- tmp/script.sh
+++ tmp/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-PGPASSWORD=space64#. psql -U pace -h localhost -d pace -w -c "COPY movimiento(cedula, codigo, tipo_movimiento_id, monto, f_contable, f_creacion, usr_creacion) FROM '$1.csv' DELIMITER ';' CSV HEADER;"
+PGPASSWORD=dg2010 psql -U postgres -h localhost -d pace -w -c "COPY movimiento(cedula, codigo, tipo_movimiento_id, monto, f_contable, f_creacion, usr_creacion) FROM '$1.csv' DELIMITER ';' CSV HEADER;"
 
 echo "proceso exitoso"
