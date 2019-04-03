f741dc148b9c1c537ccd8ca9c8e2d7695a3b1264
Mary Quiñones <marycq@gmail.com>
2019-04-03 11:41:20 -0400

    Float 2 decimal VIII

--- application/modules/panel/models/kernel/KCargador.php
+++ application/modules/panel/models/kernel/KCargador.php
@@ -443,8 +443,8 @@ class KCargador extends CI_Model{
 
     if($tipo == 2){
       //$comando = "cd tmp/; awk -F';' '{ for (x=1; x<=34; x++) {  printf \"%s;\", $x } printf \"\n\" }' " . $archivo . ".csv >>  " . $file . "/" . $file . ".csv";
-      $comando = 'cd tmp/; awk -F\';\' \'{ for (x=1; x<=34; x++) {  printf "%s;", $x } printf %$' .
-      $columna . $porcen . '.2f "\n" } \' ' . $archivo . '.csv >> ' . $file . '/' . $file . '.csv';
+      $comando = 'cd tmp/; awk -F\';\' \'{ for (x=1; x<=34; x++) {  printf "%s;", $f } printf $' .
+      $columna . $porcen . ' "\n" } \' ' . $archivo . '.csv >> ' . $file . '/' . $file . '.csv';
       exec($comando, $firma);
     }else if($tipo == 1){
       //**SE MODIFICO INCLUYENDO CONDICION PARA QUE MONTO DA<0 NO SALGA EN EL REPORTE FINAL
